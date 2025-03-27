d77fdfc895fc4213e7733aaf030ab630b7d519fc
Himanshu Singh <hsnice16@gmail.com>
2025-03-28 00:13:01 +0530

    update web show

--- src/data/web-shows.ts
+++ src/data/web-shows.ts
@@ -1,37 +1,40 @@
-import ItaewonClass from "@/web-show/itaewon-class.webp";
-import GyeongseongCreature from "@/web-show/gyeongseong-creature.jpg";
-import TheMentalist from "@/web-show/the-mentalist.jpg";
-import From from "@/web-show/from.jpg";
-import FirstLove from "@/web-show/first-love.jpg";
-import ItsOkayToNotBeOkay from "@/web-show/its-okay-to-not-be-okay.jpg";
-import FightForMyWay from "@/web-show/fight-for-my-way.jpg";
-import HighSchoolReturnOfAGangster from "@/web-show/high-school-return-of-a-gangster.jpg";
-import Shogun from "@/web-show/shogun.jpg";
-import QueenOfTears from "@/web-show/queen-of-tears.jpg";
-import TokyoVice from "@/web-show/tokyo-vice.jpg";
-import StartUp from "@/web-show/start-up.jpg";
-import Reacher from "@/web-show/reacher.webp";
-import CrashLandingOnYou from "@/web-show/crash-landing-on-you.jpg";
-import BigMouth from "@/web-show/big-mouth.webp";
-import Doona from "@/web-show/doona.jpg";
-import SongOfTheBandits from "@/web-show/song-of-the-bandits.jpg";
-import MrRobot from "@/web-show/mr-robot.jpg";
-import Warrior from "@/web-show/warrior.jpg";
-import Bloodhounds from "@/web-show/bloodhounds.jpg";
-import BlackKnight from "@/web-show/black-knight.jpg";
-import Adamas from "@/web-show/adamas.avif";
-import Suits from "@/web-show/suits.webp";
+import Riverdale from "@/web-show/riverdale.jpg";
+import TheBigBangTheory from "@/web-show/the-big-bang-theory.jpg";
+import TheVampireDiaries from "@/web-show/the-vampire-diaries.jpg";
+import SiliconValley from "@/web-show/silicon-valley.webp";
+import SquidGame from "@/web-show/squid-game.jpeg";
+import StrangerThings4 from "@/web-show/stranger-things-4.jpg";
+import AliceInBorderland from "@/web-show/alice-in-borderland.jpg";
 import PeakyBlinders from "@/web-show/peaky-blinders.jpg";
 import Rangbaaz from "@/web-show/rangbaaz.jpg";
-import AliceInBorderland from "@/web-show/alice-in-borderland.jpg";
-import StrangerThings4 from "@/web-show/stranger-things-4.jpg";
-import SquidGame from "@/web-show/squid-game.jpeg";
-import SiliconValley from "@/web-show/silicon-valley.webp";
-import TheVampireDiaries from "@/web-show/the-vampire-diaries.jpg";
-import TheBigBangTheory from "@/web-show/the-big-bang-theory.jpg";
-import Riverdale from "@/web-show/riverdale.jpg";
+import Suits from "@/web-show/suits.webp";
+import Adamas from "@/web-show/adamas.avif";
+import BlackKnight from "@/web-show/black-knight.jpg";
+import Bloodhounds from "@/web-show/bloodhounds.jpg";
+import Warrior from "@/web-show/warrior.jpg";
+import MrRobot from "@/web-show/mr-robot.jpg";
+import SongOfTheBandits from "@/web-show/song-of-the-bandits.jpg";
+import Doona from "@/web-show/doona.jpg";
+import BigMouth from "@/web-show/big-mouth.webp";
+import CrashLandingOnYou from "@/web-show/crash-landing-on-you.jpg";
+import Reacher from "@/web-show/reacher.webp";
+import StartUp from "@/web-show/start-up.jpg";
+import TokyoVice from "@/web-show/tokyo-vice.jpg";
+import QueenOfTears from "@/web-show/queen-of-tears.jpg";
+import Shogun from "@/web-show/shogun.jpg";
+import HighSchoolReturnOfAGangster from "@/web-show/high-school-return-of-a-gangster.jpg";
+import FightForMyWay from "@/web-show/fight-for-my-way.jpg";
+import ItsOkayToNotBeOkay from "@/web-show/its-okay-to-not-be-okay.jpg";
+import FirstLove from "@/web-show/first-love.jpg";
+import From from "@/web-show/from.jpg";
+import TheMentalist from "@/web-show/the-mentalist.jpg";
+import GyeongseongCreature from "@/web-show/gyeongseong-creature.jpg";
+import ItaewonClass from "@/web-show/itaewon-class.webp";
+
+import WeekHeroClassOne from "@/web-show/week-hero-class-one.jpg";
 
 export const WEB_SHOWS = {
+  "Mar '25": [{ img: WeekHeroClassOne }],
   Previous: [
     {
       img: ItaewonClass,
