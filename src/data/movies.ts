d46f0331f277ac262a778fe40c3eb962ea6cf312
Himanshu Singh <hsnice16@gmail.com>
2025-06-14 23:13:08 +0530

    update movie

--- src/data/movies.ts
+++ src/data/movies.ts
@@ -168,9 +168,14 @@ import TheAccountantTwo from "@/movie/the-accountant-two.jpg";
 import ClownInACronfield2025 from "@/movie/clown-in-a-cronfield-2025.jpeg";
 import Chime2024 from "@/movie/chime-2024.jpg";
 import TheAmateur2025 from "@/movie/the-amateur-2025.jpg";
+import WinterSpringSummerOrFall from "@/movie/winter-spring-summer-or-fall.jpg";
 
 export const MOVIES = {
   "Jun '25": [
+    {
+      name: "Winter Spring Summer or Fall (2024)",
+      img: WinterSpringSummerOrFall,
+    },
     {
       name: "The Amateur (2025)",
       img: TheAmateur2025,
